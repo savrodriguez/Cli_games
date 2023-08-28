46543a8c6b89a3cd2c86383230f2e2a9e242574b
Savana <rodriguezsavana543@gmail.com>
2023-08-28 15:51:26 -0700

    cli_games

---
+++ Cli_games_1100/bear,ninja,cowboy/bnc.py
@@ -0,0 +1,30 @@
+#cli-games/bnc.py
+
+from random import randint 
+roles = ["Bear","Ninja","Cowboy"]
+computer = roles[randint(0,2)]
+player = input("Bear, Ninja, or Cowboy? > ")
+print(player,computer)
+if computer == player:
+    print("DRAW!")
+elif computer == "Cowboy":
+    if player == "Bear":
+        print("You lose!", player, "is shot by", computer)
+    else:
+        print("You win!", player, "defeats", computer)
+elif computer == "Bear":
+    if player == "Cowboy":
+        print("You win!",player, "shoots", computer)
+    else:
+        print("You lose!", player, "is eaten by", computer)
+elif computer == "Ninja":
+    if player == "Cowboy":
+        print("You lose!", player, "is defeated by", computer)
+    else:
+        print("You win!", player,"eats",computer)
+play_again = input("Would you like to play again? (YES/NO) > ")
+if play_again == 'yes':
+    player = False
+    computer = roles[randint(0,2)]
+else:
+    breakpoint
---
+++ Cli_games_1100/flash-cards/flashcards.py
@@ -0,0 +1,20 @@
+import json
+with open('me-capitals.json','r') as f:
+    data = json.load(f)
+    for i in data["cards"]:
+        print(i)
+import json
+with open('me-capitals.json','r') as f:
+    data = json.load(f)
+    total = len(data["cards"])
+    score = 0
+    for i in data["cards"]:
+        guess = input(i["q"]+">")
+        if guess.lower() == i["a"].lower():
+            score += 1
+            print(f"Correct! Current Score: {score}/{total}")
+        else:
+            print("Incorrect! The correct answer was"
+                  , i["a"])
+            print(f"Current score: {score}/{total}")
+
